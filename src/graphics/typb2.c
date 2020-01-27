784501ab341cfeccaed8362509af393683a2a91b
Alexander Linder <alexander-linder@gmx.de>
2020-01-27 13:24:11 +0100

    Add typ-b.2 example

--- src/graphics/typb2.c
+++ src/graphics/typb2.c
@@ -1,1 +1,9 @@
-//TODO ADD CODE FRAGMENT FOR RULE
+...
+int speed;
+if (some Condition) {
+    speed = 101;
+}
+if (speed > 100) { /* Speed wurde mglw nie initialisiert */
+    printf("Sie fahren zu schnell")
+}
+...
